df2ee1769f758c13e6e64e834b9fd437d378e853
Mohan Vamsi <mohanvamsi.1155@gmail.com>
2025-07-30 00:54:57 +0530

    logger fix for sonar

--- app/helper/utils/logger.py
+++ app/helper/utils/logger.py
@@ -3,31 +3,31 @@ import logging
 
 class Logger:
     def __init__(self, name: str = "default_logger", level: str = "DEBUG"):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
+        self.log = logging.getLogger(name)
+        self.log.setLevel(level)
 
         # Avoid adding multiple handlers if logger already initialized
-        if not self.logger.handlers:
+        if not self.log.handlers:
             console_handler = logging.StreamHandler()
             console_handler.setFormatter(
                 logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s")
             )
-            self.logger.addHandler(console_handler)
+            self.log.addHandler(console_handler)
 
     def info(self, msg: str, *args, **kwargs):
-        self.logger.info(msg, *args, **kwargs)
+        self.log.info(msg, *args, **kwargs)
 
     def debug(self, msg: str, *args, **kwargs):
-        self.logger.debug(msg, *args, **kwargs)
+        self.log.debug(msg, *args, **kwargs)
 
     def warning(self, msg: str, *args, **kwargs):
-        self.logger.warning(msg, *args, **kwargs)
+        self.log.warning(msg, *args, **kwargs)
 
     def error(self, msg: str, *args, **kwargs):
-        self.logger.error(msg, *args, **kwargs)
+        self.log.error(msg, *args, **kwargs)
 
     def exception(self, msg: str, *args, **kwargs):
-        self.logger.exception(msg, *args, **kwargs)
+        self.log.exception(msg, *args, **kwargs)
 
     def set_level(self, level: int):
-        self.logger.setLevel(level)
+        self.log.setLevel(level)
